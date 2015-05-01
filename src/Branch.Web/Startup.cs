d0948e49c9703d9a9cecc5eeb67b6d648c5e5e4c
Alex Forbes-Reed <alexreed@outlook.com>
2015-05-01 12:12:33 +0100

    removed legacy

--- src/Branch.Web/Startup.cs
+++ src/Branch.Web/Startup.cs
@@ -58,9 +58,6 @@ namespace Branch.Web
 				routes.MapRoute(
 					name: "default",
 					template: "{controller=Home}/{action=Index}/{id?}");
-
-				// Uncomment the following line to add a route for porting Web API 2 controllers.
-				// routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
 			});
 		}
 	}
