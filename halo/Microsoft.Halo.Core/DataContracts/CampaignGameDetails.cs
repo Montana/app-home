0d6a79d145df74778206b228ff2ef7ec02514308
Alex Forbes-Reed <alexreed@outlook.com>
2015-11-10 13:42:36 +0000

    added campaigngamedetails model - prepping for campaign game viewing

---
+++ halo/Microsoft.Halo.Core/DataContracts/CampaignGameDetails.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Halo.Core.DataContracts.Abstracts;
+using Microsoft.Halo.Core.DataContracts.Enums;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Microsoft.Halo.Core.DataContracts
+{
+	public class CampaignGameDetails
+		: GameDetails
+	{
+		[JsonProperty("Mission")]
+		public int Mission { get; set; }
+
+		[JsonProperty("CampaignScoringEnabled")]
+		public bool CampaignScoringEnabled { get; set; }
+
+		[JsonProperty("CampaignGlobalScore")]
+		public int CampaignGlobalScore { get; set; }
+
+		[JsonProperty("SkullIds")]
+		public IReadOnlyCollection<int> SkullIds { get; set; }
+
+		[JsonProperty("Difficulty")]
+		public Difficulty Difficulty { get; set; }
+	}
+}
