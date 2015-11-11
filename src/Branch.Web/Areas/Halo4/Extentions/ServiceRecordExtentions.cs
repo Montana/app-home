c7ef29e547eb28d320ce70f7d599e01bf60b73f0
Alex Forbes-Reed <alexreed@outlook.com>
2015-11-11 12:33:48 +0000

    fixed #65

--- src/Branch.Web/Areas/Halo4/Extentions/ServiceRecordExtentions.cs
+++ src/Branch.Web/Areas/Halo4/Extentions/ServiceRecordExtentions.cs
@@ -6,19 +6,21 @@ namespace Branch.Web.Areas.Halo4.Extentions
 {
 	public static class ServiceRecordExtentions
 	{
-		public static string ToJavascriptArray(this IReadOnlyCollection<GameModeDetailsFull> gameModeDetails)
+		public static string ToJavascriptArray(this IReadOnlyCollection<GameModeDetailsFull> gameModeDetails, string[] colours)
 		{
 			var javascriptObjectArray = new List<object>();
 
+			var index = 0;
 			foreach (var gameModeDetail in gameModeDetails)
 			{
 				javascriptObjectArray.Add(new
 				{
 					value = gameModeDetail.TotalGamesStarted,
-					color = "#ccc",
-					highlight = "#bbb",
+					color = colours[index],
 					label = gameModeDetail.Name
 				});
+
+				index++;
 			}
 
 			return JsonConvert.SerializeObject(javascriptObjectArray, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.None });
