1b8d3eb5fa73fbff3f6a5c6da9b36e9f16cf59b3
Alex Forbes-Reed <alexreed@outlook.com>
2015-11-18 19:32:38 +0000

    wrote logic for halo 5 map and game variant metadata

--- src/Branch.Service.Halo5/Services/MetadataService.cs
+++ src/Branch.Service.Halo5/Services/MetadataService.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Branch.Service.Xuid.Services;
 using Branch.Service.Halo5.Models.Api;
+using Branch.Service.Halo5.Database.Repositories.Interfaces;
 
 namespace Branch.Service.Halo5.Services
 {
@@ -16,15 +17,24 @@ namespace Branch.Service.Halo5.Services
 	{
 		public MetadataService(ILoggerFactory loggerFactory, HttpManagerService httpManagerService,
 			XuidLookupService xuidLookupService, Halo5DbContext halo5DbContext, Halo5DdbRepository halo5DdbRepository,
-			AuthenticationService authenticationService)
+			AuthenticationService authenticationService, IMapVariantRepository mapVariantRepository, IGameVariantRepository gameVariantRepository)
 			: base(
 				loggerFactory, httpManagerService, xuidLookupService, halo5DbContext, halo5DdbRepository, authenticationService)
-		{ }
-		
+		{
+			_mapVariantRepository = mapVariantRepository;
+			_gameVariantRepository = gameVariantRepository;
+		}
+
+		private readonly IMapVariantRepository _mapVariantRepository;
+		private readonly IGameVariantRepository _gameVariantRepository;
+
 		private const string GetMetadataUrl = "https://www.haloapi.com/metadata/h5/metadata/{0}";
+		private const string GetMetadataExtendedUrl = "https://www.haloapi.com/metadata/h5/metadata/{0}/{1}";
 		private const string SpartanRanksMetadataSlug = "spartan-ranks";
 		private const string MapsMetadataSlug = "maps";
+		private const string MapVariantMetadataSlug = "map-variants";
 		private const string GameBaseVariantsMetadataSlug = "game-base-variants";
+		private const string GameVariantMetadataSlug = "game-variants";
 
 		private readonly TimeSpan _cacheRefreshTime = new TimeSpan(0, 30, 0);
 		private readonly TimeSpan _localCacheRefreshTime = TimeSpan.FromDays(100);
@@ -85,6 +95,44 @@ namespace Branch.Service.Halo5.Services
 			return MapMetadata;
 		}
 
+		public async Task<MapVariant> GetMapVariantMetadataAsync(Guid mapVariantId)
+		{
+			var mapVariant = _mapVariantRepository.Where(mv => mv.MapVariantId == mapVariantId).FirstOrDefault();
+			if (mapVariant != null && mapVariant.UpdatedAt + _localCacheRefreshTime > DateTime.UtcNow)
+				return new MapVariant
+				{
+					Id = mapVariant.MapVariantId,
+					ContentId = mapVariant.ContentId,
+					Description = mapVariant.Description,
+					MapId = mapVariant.MapId,
+					MapImageUrl = mapVariant.MapImageUrl,
+					Name = mapVariant.Name
+				};
+
+			// Populate template metadata url
+			var getMapVariantMetadataUri = new Uri(string.Format(GetMetadataExtendedUrl, MapVariantMetadataSlug, mapVariantId));
+
+			// Get Map Variant Metadata from 343's Halo Service
+			var mapVariantMetadataResponse = await HttpManagerService.ExecuteRequestAsync<MapVariant>(HttpMethod.GET, getMapVariantMetadataUri,
+				headers: new Dictionary<string, string>
+				{
+					{ "Ocp-Apim-Subscription-Key", AuthenticationService.GetAuthentication() }
+				});
+
+			if (mapVariant == null)
+				mapVariant = new Database.Models.MapVariant();
+
+			mapVariant.ContentId = mapVariantMetadataResponse.ContentId;
+			mapVariant.Description = mapVariantMetadataResponse.Description;
+			mapVariant.MapId = mapVariantMetadataResponse.MapId;
+			mapVariant.MapImageUrl = mapVariantMetadataResponse.MapImageUrl;
+			mapVariant.MapVariantId = mapVariantMetadataResponse.Id;
+			mapVariant.Name = mapVariantMetadataResponse.Name;
+			_mapVariantRepository.Update(mapVariant);
+
+			return mapVariantMetadataResponse;
+        }
+
 		public async Task<Response<GameBaseVariant>> GetGameBaseVariantsMetadataAsync()
 		{
 			if (GameBaseVariantMetadata != null)
@@ -109,6 +157,44 @@ namespace Branch.Service.Halo5.Services
 			};
 
 			return GameBaseVariantMetadata;
+		}
+
+		public async Task<GameVariant> GetGameVariantMetadataAsync(Guid gameVariantId)
+		{
+			var gameVariant = _gameVariantRepository.Where(gv => gv.GameVariantId == gameVariantId).FirstOrDefault();
+			if (gameVariant != null && gameVariant.UpdatedAt + _localCacheRefreshTime > DateTime.UtcNow)
+				return new GameVariant
+				{
+					Id = gameVariant.GameVariantId,
+					ContentId = gameVariant.ContentId,
+					Description = gameVariant.Description,
+					GameBaseVariantId = gameVariant.GameBaseVariantId,
+					IconUrl = gameVariant.IconUrl,
+					Name = gameVariant.Name
+				};
+
+			// Populate template metadata url
+			var getGameVariantMetadataUri = new Uri(string.Format(GetMetadataExtendedUrl, GameVariantMetadataSlug, gameVariantId));
+
+			// Get Game Variant Metadata from 343's Halo Service
+			var gameVariantMetadataResponse = await HttpManagerService.ExecuteRequestAsync<GameVariant>(HttpMethod.GET, getGameVariantMetadataUri,
+				headers: new Dictionary<string, string>
+				{
+					{ "Ocp-Apim-Subscription-Key", AuthenticationService.GetAuthentication() }
+				});
+
+			if (gameVariant == null)
+				gameVariant = new Database.Models.GameVariant();
+
+			gameVariant.ContentId = gameVariantMetadataResponse.ContentId;
+			gameVariant.Description = gameVariantMetadataResponse.Description;
+			gameVariant.GameBaseVariantId = gameVariantMetadataResponse.GameBaseVariantId;
+			gameVariant.IconUrl = gameVariantMetadataResponse.IconUrl;
+			gameVariant.GameVariantId = gameVariantMetadataResponse.Id;
+			gameVariant.Name = gameVariantMetadataResponse.Name;
+			_gameVariantRepository.Update(gameVariant);
+
+			return gameVariantMetadataResponse;
 		}
 	}
 }
