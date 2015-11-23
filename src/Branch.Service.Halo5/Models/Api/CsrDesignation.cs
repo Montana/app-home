bfc9c8b53beec2b1888180f175fbdecc0991dfc9
Alex Forbes-Reed <alexreed@outlook.com>
2015-11-23 20:10:52 +0000

    added halo 5 csr designation models

---
+++ src/Branch.Service.Halo5/Models/Api/CsrDesignation.cs
@@ -0,0 +1,26 @@
+﻿using Branch.Service.Halo5.Models.Api.Abstracts;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Branch.Service.Halo5.Models.Api
+{
+	public class CsrDesignation
+		: Result
+	{
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("bannerImageUrl")]
+		public string BannerImageUrl { get; set; }
+
+		[JsonProperty("tiers")]
+		public IReadOnlyCollection<CsrDesignationTier> Tiers { get; set; }
+
+		[JsonProperty("id")]
+		public int Id { get; set; }
+
+		[JsonProperty("contentId")]
+		public Nullable<Guid> ContentId { get; set; }
+	}
+}
---
+++ src/Branch.Service.Halo5/Models/Api/CsrDesignationTier.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Branch.Service.Halo5.Models.Api
+{
+	public class CsrDesignationTier
+	{
+		[JsonProperty("iconImageUrl")]
+		public string IconImageUrl { get; set; }
+
+		[JsonProperty("id")]
+		public int Id { get; set; }
+
+		[JsonProperty("contentId")]
+		public Nullable<Guid> ContentId { get; set; }
+	}
+}
