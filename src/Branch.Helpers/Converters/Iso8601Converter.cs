350d22cb4284068415e13829f84766986abf01c9
Alex Forbes-Reed <alexreed@outlook.com>
2015-12-03 16:37:04 +0000

    [helpers] added Iso8601 Timespan converter

---
+++ src/Branch.Helpers/Converters/Iso8601Converter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace Branch.Helpers.Converters
+{
+	public class Iso8601Converter : JsonConverter
+	{
+		public override bool CanConvert(Type objectType)
+		{
+			return true;
+		}
+
+		public override bool CanWrite
+		{
+			get { return false; }
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			string timespan = reader.Value.ToString();
+			return XmlConvert.ToTimeSpan(timespan);
+		}
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			string timespan;
+			if (value is TimeSpan)
+				timespan = XmlConvert.ToString((TimeSpan)value);
+			else
+				throw new Exception("Expected timespan object value.");
+
+			writer.WriteValue(timespan);
+		}
+	}
+}
